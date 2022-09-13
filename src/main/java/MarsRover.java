9931ad9fe6bae5f99fef0dff711fbfe2d99b507e
Meryem Sel <mzbasel637936@gmail.com>
2022-09-13 11:45:38 +0100

    turn left method

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,8 +1,22 @@
 public class MarsRover {
     public String rotateRight(String position) {
-        if (position == "N") {
+        if (position.equals("N")) {
             return "0:0:E";
         }
-        return "0:0:S";
+        if (position.equals("E")) {
+            return "0:0:S";
+        }
+        if (position.equals("S")) {
+            return "0:0:W";
+        }
+        return "0:0:N";
+    }
+
+    public String rotateLeft(String position) {
+        switch (position){
+            case "N": position = "W";
+        }
+        return "0:0:"+position;
+
     }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,32 +1,39 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
-    @Test
-    public void turn_right() {
+
+    @ParameterizedTest
+    @CsvSource({
+            "N, '0:0:E'",
+            "E, '0:0:S'",
+            "S, '0:0:W'",
+            "W, '0:0:N'"
+    })
+    public void turn_right(String position, String expectedCoordinates) {
         MarsRover rover = new MarsRover();
 
-        String result = rover.rotateRight("N");
+        String result = rover.rotateRight(position);
 
-        assertEquals("0:0:E", result);
+        assertEquals(expectedCoordinates, result);
     }
 
-    @Test
-    public void turn_right_twice() {
+    @ParameterizedTest
+    @CsvSource({
+            "N, '0:0:W'",
+            "E, '0:0:N'",
+            "S, '0:0:E'",
+            "W, '0:0:S'"
+    })
+    public void turn_left(String position, String expectedCoordinates) {
         MarsRover rover = new MarsRover();
 
-        String result = rover.rotateRight("E");
+        String result = rover.rotateLeft(position);
 
-        assertEquals("0:0:S", result);
+        assertEquals(expectedCoordinates, result);
     }
 
-    @Test
-    public void turn_right_from_south_to_west() {
-        MarsRover rover = new MarsRover();
-
-        String result = rover.rotateRight("S");
-
-        assertEquals("0:0:W", result);
-    }
 }
